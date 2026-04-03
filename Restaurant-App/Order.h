5c3fb076cee9875c51cf4173bbb1e1b852c5c691
Adham Ehab <adhamehab2112@gmail.com>
2026-04-03 14:53:51 +0200

    UI code updates

--- Restaurant-App/Order.h
+++ Restaurant-App/Order.h
@@ -1,23 +1,22 @@
 #pragma once
 #include <iostream>
 
-// أنواع الأوردرات المتاحة في المطعم
+
 enum ORD_TYPE { TYPE_NRM, TYPE_VGAN, TYPE_VIP };
 
 class Order {
 private:
-    int ID;             // رقم الأوردر
-    ORD_TYPE type;      // نوع الأوردر (عادي، نباتي، VIP)
-    int ArrivalTime;    // وقت وصول الأوردر
+    int ID;             
+    ORD_TYPE type;      
+    int ArrivalTime;    
 
 public:
-    // الـ Constructor عشان ندي قيم للأوردر أول ما يتخلق
+    
     Order(int id, ORD_TYPE t, int AT) : ID(id), type(t), ArrivalTime(AT) {}
 
-    // دالة بترجع الـ ID لو احتجناه في مكان تاني
     int getID() const { return ID; }
 
-    // الجزء السحري: ده اللي بيخلي الـ cout تطبع الـ ID على طول لما نديها الأوردر
+    
     friend std::ostream& operator<<(std::ostream& os, const Order* ord) {
         if (ord) os << ord->ID;
         return os;
